b77994144a03fbb956b0c66fe266be2b2a244d91
Tubalevião <alvaro.prata.filho@gmail.com>
2018-02-28 01:23:16 +0000

    finishing test folder code

--- test/app.js
+++ test/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var up = require('../server.js'); // you should use 'up.io'
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
